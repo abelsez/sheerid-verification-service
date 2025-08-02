8cbfab099edbbc8c10a0b737b9214ef662c26b77
abelsez <abelsez@example.com>
2025-08-01 22:13:07 -0400

    Fix: Updated base path for custom domain deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sheerid-verification-service/',
+  base: '/',
   publicDir: 'public',
   plugins: [react(),tailwindcss()],
   resolve: {
